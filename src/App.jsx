0bcd544471fa13111f94011f819ac59768afd6bd
Rachael Njoroge <wangoinjoroge450@gmail.com>
2024-06-06 17:12:03 +0300

    Made modofications on the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from './NavBar'
+import Hero from './Hero'
 
 function App() {
   return (
     <>
-      <NavBar />
+      {/* <NavBar /> */}
+      <Hero />
     </>
   )
 }
---
+++ src/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <>
+        <div id='hero'>
+            <h1>Hi I'm Wangui, I design and develop web applications</h1>
+            <h5></h5>
+        </div>
+    </>
+  )
+}
+
+export default Hero
