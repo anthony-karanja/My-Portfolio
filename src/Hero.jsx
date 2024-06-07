f4f6f6321ffbf517144adaefb9d3fac5a6d0280c
Rachael Njoroge <wangoinjoroge450@gmail.com>
2024-06-07 22:58:08 +0300

    Worked on the hero section

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -6,6 +6,7 @@ function Hero() {
         <div id='hero'>
             <h1>Hi I'm Wangui, I design and develop web applications</h1>
             <h5></h5>
+            <img src="/Biggerprofile.png" alt="" />
         </div>
     </>
   )
