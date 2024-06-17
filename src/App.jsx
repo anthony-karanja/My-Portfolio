eea124950e1197f145e245e32d76f5aec07ab44c
Rachael Njoroge <wangoinjoroge450@gmail.com>
2024-06-17 22:56:41 +0300

    fixed the circles on the hero section and added the logo on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 import NavBar from './NavBar'
 import Hero from './Hero'
 import Projects from './Projects'
+import Services from './Services';
 
 function App() {
   return (
     <>
       <NavBar />
       <Hero />
+      <Services />
       <Projects />
     </>
   )
--- src/Services.jsx
+++ src/Services.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card';
 
 function Services() {
   return (
     <>
-        <h1>What I do</h1>
+      <h1>What I do</h1>
+      <div id='projects'>
+        <Card style={{ width: '18rem' }} className='project1'>
+          <Card.Body>
+            <Card.Title>Front-End Developer</Card.Title>
+              <Card.Text>
+                React
+              </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }} className='project1'>
+          <Card.Body>
+            <Card.Title>UI/UX Designer</Card.Title>
+              <Card.Text>
+                React
+              </Card.Text>
+          </Card.Body>
+        </Card>
+
+      </div>
     </>
   )
 }
