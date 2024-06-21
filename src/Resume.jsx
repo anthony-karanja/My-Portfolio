44f991745c96085fc1f4a0831f0a060c3b65425c
Rachael Njoroge <wangoinjoroge450@gmail.com>
2024-06-21 23:28:20 +0300

    Installed a new pdfviewer library for the resume component

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import { Document, Page } from 'react-pdf'
 import NavBar from './NavBar'
+import { Viewer } from '@react-pdf-viewer/core';
 // import pdf from 'public/Resume-Wangui.pdf'
 
 function Resume() {
   return (
     <>
-        <div id='resume'>
+      <div>
+        <Viewer fileUrl="/Resume-Wangui.pdf" />
+      </div>
+        {/* <div id='resume'>
             <NavBar />
             <h2>Resume</h2>
             <Document file='public\Resume-Wangui.pdf' >
                 <Page pageNumber={1}/>
             </Document>
-        </div>
+        </div> */}
     </>
   )
 }
